34cfa8c588e290e8c01101fa9d840af66fe6d20d
Tony Ganch <tonyganch@gmail.com>
2015-07-02 01:02:47 +0300

    Report errors by mail

--- comb.js
+++ comb.js
@@ -1,3 +1,24 @@
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport();
+
+function reportError(subject, text) {
+  transporter.sendMail({
+    from: 'bugs@csscomb.com',
+    to: 'tonyganch+csscomb@gmail.com',
+    subject: subject,
+    text: text,
+    html: '<pre>' + text + '</pre>'
+  });
+}
+
+function reportUnacceptedReferer(referer) {
+  reportError('Unaccepted referer', referer);
+}
+
+function reportParsingError(css, error) {
+  reportError('Parsing error', error.stack + '\n\n\n' + css);
+}
+
 module.exports = function(req, res){
     var Comb = require('csscomb'),
         comb = new Comb('csscomb'),
@@ -12,13 +33,14 @@ module.exports = function(req, res){
         'http://csscomb.com/online',
     ];
     if (acceptedReferers.indexOf(req.headers.referer) < 0)
-        res.send('Don\'t be evil');
+        reportUnacceptedReferer(req.headers.referer);
 
     // Comb it, baby:
     try {
         output = comb.processString(input);
     } catch (e) {
         output = e.message;
+        reportParsingError(input, e);
     }
 
     res.send(output);
